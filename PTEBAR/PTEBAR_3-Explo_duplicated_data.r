93f5d4d7352c4ba9c54bb7b18374abc111c3bc77
unknown <Etudiant@5357320350.campus.univ.run>
2021-07-09 14:08:47 +0400

    # Explo duplicated rows - final

--- PTEBAR/PTEBAR_3-Explo_duplicated_data.r
+++ PTEBAR/PTEBAR_3-Explo_duplicated_data.r
@@ -4,6 +4,7 @@
 
 rm(list = ls())
 require(sf)
+require(dplyr)
 
 setwd("C:/Users/Etudiant/Desktop/SMAC/GITHUB/SMAC-ENTROPIE_tracking/PTEBAR")
 source('C:/Users/Etudiant/Desktop/SMAC/GITHUB/SMAC-ENTROPIE_tracking/PTEBAR/PTEBAR_0-Functions_for_scripts.R')
@@ -25,6 +26,9 @@ gps_dupl_id <- unique(gps1_noNA$LoggerTime[duplicated(gps1_noNA$LoggerTime)])
 # Extraction of the duplicated database
 gps_dupl <- gps1_noNA[gps1_noNA$LoggerTime %in% gps_dupl_id,]
 
+tot <- dplyr::count(gps_dupl, gps_dupl$LoggerTime)
+barplot(table(table(gps_dupl$LoggerTime)))
+
 gps_dupl_spat <- st_as_sf(gps_dupl,
                           coords = c('Longitude', 'Latitude'),
                           crs = 4326) # '+init=epsg:4326'
@@ -32,15 +36,21 @@ gps_dupl_spat <- st_as_sf(gps_dupl,
 mapview::mapview(gps_dupl_spat,
                  zcol = 'LoggerTime')
 
+LoggerTime <- vector(length = length(gps_dupl_id))
+max_dist <- vector(length = length(gps_dupl_id))
+n_dupl <- vector(length = length(gps_dupl_id))
 
-bobynco <- vector(length = length(gps_dupl_id))
 for(i in 1:length(gps_dupl_id)){
-  boby <- max(st_distance(gps_dupl_spat[gps_dupl_spat$LoggerTime == gps_dupl_id[i],]))
   
-  bobynco[i] <- boby
+  dat <- gps_dupl_spat[gps_dupl_spat$LoggerTime == gps_dupl_id[i],]
+  
+  LoggerTime[i] <- gps_dupl_id[i]
+  n_dupl[i] <- nrow(dat)
+  max_dist[i] <- max(st_distance(dat))
 }
 
-table(bobynco)
+info_dupl <- data.frame(LoggerTime, n_dupl, max_dist)
+
 
 
 
