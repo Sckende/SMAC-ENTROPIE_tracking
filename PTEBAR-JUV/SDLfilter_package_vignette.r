21cbac29f2edf4788b30abfa0efe58da84064668
Claire-Cecile Juhasz <ccjuhasz@campus.univ.run>
2021-09-17 14:52:17 +0400

    # test

--- PTEBAR-JUV/SDLfilter_package_vignette.r
+++ PTEBAR-JUV/SDLfilter_package_vignette.r
@@ -2,6 +2,9 @@
 # https://www.rdocumentation.org/packages/SDLfilter/versions/2.1.1 #
 ###################################################################
 library(SDLfilter)
+library(sf)
+library(mapview)
+library(lubridate)
 
 # Loading data 
 data(turtle)
@@ -9,6 +12,12 @@ names(turtle)
 head(turtle)
 class(turtle$DateTime)
 
+# Visualization
+turtle_sp <- st_as_sf(turtle,
+                      coords = c('lon', 'lat'))
+mapview(turtle_sp,
+        zcol = 'id',
+        burst = T)
 # Remove temporal and spatial duplicates
 dim(turtle)
 dup <- turtle[duplicated(turtle$DateTime),]
@@ -22,6 +31,55 @@ V <- vmax(turtle.dup)
 
 # Calculate the maximum one-way linear speed of a loop trip
 VLP <- vmaxlp(turtle.dup) 
+
+## Run ddfilter
+turtle.dd <- ddfilter(turtle.dup,
+                      vmax = V,
+                      vmaxlp = VLP)
+
+#### ---------- Application on PTEBAR data ---------- ####
+argos <- read.table("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/DATA/PTEBAR_JUV_TRACK_argos.txt",
+                    h = T,
+                    sep = '\t',
+                    dec = '.')
+
+head(argos)
+
+argos$Date <- as.POSIXct(argos$Date,
+                         format = "%d/%m/%Y %H:%M") # Date format
+
+argos$DATE_1 <- as.POSIXct(argos$DATE_1,
+                           format = "%d/%m/%Y %H:%M") # Date format
+
+class(argos$Date)
+summary(argos$Date)
+barplot(table(argos$Date))
+year(head(argos$Date))
+table(year(argos$Date))
+summary(argos)
+
+# Name modifications for functions in package SDLfilter
+names(argos)[1:5] <- c('id', 'DateTime', 'lat', 'lon', 'qi')
+
+# Remove temporal and spatial duplicates
+argos.dup <- dupfilter(argos)
+dim(argos.dup)
+
+projcrs <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+argos.dup.sp <- st_as_sf(argos.dup,
+                         coords = c('lon', 'lat'),
+                         crs = projcrs)
+argos.dup.sp$id <- as.factor(argos.dup.sp$id)
+
+mapview(argos.dup.sp,
+        zcol = 'id',
+        burst = T)
+
+# Calculate the maximum linear speed between two consecutive locations
+V <- vmax(argos.dup)  
+
+# Calculate the maximum one-way linear speed of a loop trip
+VLP <- vmaxlp(argos.dup) 
 
 ## Run ddfilter
 turtle.dd <- ddfilter(turtle.dup,
