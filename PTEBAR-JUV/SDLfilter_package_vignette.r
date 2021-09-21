37524d3ff8877dbd390b4bc9c5e484b676ff30de
Claire-Cecile Juhasz <ccjuhasz@campus.univ.run>
2021-09-21 12:43:21 +0400

    # adehabitatHR vignettes

--- PTEBAR-JUV/SDLfilter_package_vignette.r
+++ PTEBAR-JUV/SDLfilter_package_vignette.r
@@ -89,28 +89,52 @@ turtle.dd <- ddfilter(turtle.dup,
                       vmax = V,
                       vmaxlp = VLP)
 
+###########################################################
+# --------- adehabitatHR package ----------------------- #
+#########################################################
+rm(list=ls())
+library(adehabitatHR)
+library(sp)
+argos <- read.table("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/DATA/PTEBAR_JUV_TRACK_argos.txt",
+                    h = T,
+                    sep = '\t',
+                    dec = '.')
 
-# -------------------------------- #
-argos.qual <- argos[argos$qi %in% c('0', '1', '2', '3', 'A', 'B'),]
-dup_argos.qual <- argos.qual[duplicated(paste(argos.qual$id, argos.qual$DateTime)),]
+head(argos)
+
+argos$Date <- as.POSIXct(argos$Date,
+                         format = "%d/%m/%Y %H:%M") # Date format
+
+argos$DATE_1 <- as.POSIXct(argos$DATE_1,
+                           format = "%d/%m/%Y %H:%M") # Date format
+
+# Data quality filer
+unique(argos$Class)
+argos.qual <- argos[argos$Class %in% c('0', '1', '2', '3', 'A', 'B'),]
+
+# Duplicated date filter
+dup_argos.qual <- argos.qual[duplicated(paste(argos.qual$Vessel, argos.qual$DateTime)),]
 
 argos.qual.sing <- argos.qual[!duplicated(argos.qual[c(1, 2)]),] # Remove duplicated rows based on ID and DateTime
-table(argos.qual.sing$id)
+table(argos.qual.sing$Vessel)
 summary(argos.qual.sing)
 
-library(adehabitatHR)
-
+# Spatial Dataframe conversion
 projcrs <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 argos.sp <- st_as_sf(argos.qual.sing,
-                     coords = c('lon', 'lat'),
-                     crs = projcrs)
+                     coords = c('Longitude', 'Latitude'),
+                     crs = projcrs) # step 1
 class(argos.sp)
 
-library(sp)
-coords <- SpatialPoints(argos.qual.sing[, c('lon', 'lat')])
+
+coords <- SpatialPoints(argos.qual.sing[, c('Longitude', 'Latitude')])
 argos.sp2 <- SpatialPointsDataFrame(coords, argos.qual.sing)
 
-# adehabitatHR - The Minimum Convex Polygon Method
+class(argos.sp2)
+
+# ------------------------------------------------- #
+# adehabitatHR - The Minimum Convex Polygon Method #
+# ----------------------------------------------- #
 
 cp <- mcp(argos.sp2[,1], percent = 95)
 class(cp)
@@ -124,4 +148,31 @@ mapview::mapview(cp,
 
 as.data.frame(cp)
 
-# adehabitatHR - Kernel method
+# Computation of home range size for various choices of the number of extreme relocations to be excluded
+x11(); mcp.area(argos.sp2[, 1],
+                percent = seq(50, 100, by = 5),
+                unin = 'km', # unit of original data -> c('m', 'km')
+                unout = 'km2', # unit of the results -> c('ha', 'km2', 'm2')
+                plotit = T)
+# ------------------------------------------------------------- #
+# adehabitatHR - Kernel estimatipon & utilization distribution #
+# ----------------------------------------------------------- #
+
+# Estimation of UD in each pixel of a grid superposed to the relocations
+kud <- kernelUD(argos.sp2[, 1],
+                h = 'href') # based on the 'reference bandwidth'
+kud
+
+x11(); image(kud)
+
+kud1 <- kernelUD(argos.sp2[, 1],
+                 h = 'LSCV') # based on the 'Least Square Cross Validation'
+
+# WARNING!! No convergence in LSCV for:
+#   [1] "162070" "162072" "162073" "166561" "166563" "166564" "166565" "166566" "166568" "166569"
+# [11] "166571" "166572" "166573"
+# Consider a new fit of UD using the ad hoc method for h.
+kud1
+
+x11(); image(kud1)
+x11(); plotLSCV(kud1)
