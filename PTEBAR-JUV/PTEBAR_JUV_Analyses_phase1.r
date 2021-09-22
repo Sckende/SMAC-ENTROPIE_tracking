7679fdf282a61a61ff04669fe39c9f3932215920
Claire-Cecile Juhasz <ccjuhasz@campus.univ.run>
2021-09-22 15:15:19 +0400

    # One map per individual with points and line

--- PTEBAR-JUV/PTEBAR_JUV_Analyses_phase1.r
+++ PTEBAR-JUV/PTEBAR_JUV_Analyses_phase1.r
@@ -13,31 +13,42 @@ library('dplyr')
 library('adehabitatHR')
 
 #### Loading and treatment of data #### 
-arg <- read.table("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/DATA/PTEBAR_JUV_TRACK_argos.txt",
+argos <- read.table("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/DATA/PTEBAR_JUV_TRACK_argos_CLEANED.txt",
                  h = T,
                  sep = '\t',
-                 dec = '.')[, -c(2, 6:9, 14:17)]
-names(arg)
-summary(arg)
+                 dec = '.')[, c(1:4, 9, 17)]
+row.names(argos) <- NULL
+head(argos)
+summary(argos)
 
-arg$Vessel <- as.factor(arg$Vessel)
-arg$DATE_1 <- as.POSIXct(arg$DATE_1,
-                           format = "%d/%m/%Y %H:%M") # Date format
-class(arg$DATE_1)
+argos$Vessel <- as.factor(argos$Vessel)
 
-# --- Deletion of low quality data
-table(arg$Class)
-arg1 <- arg[arg$Class %in% c('0', '1', '2', '3', 'A', 'B'),]
 
-# --- Check for duplicated data based on Vessel and DATE_1
-arg.list <- split(arg1, arg1$Vessel)
+class(argos$Date)
+head(argos$Date)
 
-lapply(arg.list, function(x){
-  summary(as.numeric(diff(x$DATE_1)))
+argos$Date <- as.POSIXct(argos$Date,
+                         format = '%Y-%m-%d %H:%M:%S')
+argos$deploy <- as.POSIXct(argos$deploy,
+                           format = '%Y-%m-%d %H:%M:%S') # Date format
+
+#### Map production per individuals - Overlap btw points and tracklines ####
+argos.lines.sp <- readRDS('C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/DATA/PTEBAR_JUV_track_lines_data.rds')
+class(argos.lines.sp)
+
+argos.ls <- split(argos, argos$Vessel)
+projcrs <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+
+lapply(argos.ls, function(x){
+  x.sp <- st_as_sf(x,
+                   coords = c('Longitude', 'Latitude'),
+                   crs = projcrs)
+  mapview(x.sp, popup = popupTable(x.sp, zcol = x.sp$Date)) 
+  # + mapview(argos.lines.sp[argos.lines.sp$Vessel == unique(x$Vessel),])
 })
 
-# Spatial point for each hour excepted for 162072, 166561, 166563, 166569, 166572
-# To check case by case
-# ---- 162072
-head(arg1[arg1$Vessel == '162072',]) # First date impossible -> TO DELETE
-
+mapview(x.sp,
+        popup = popupTable(x.sp[, c('Vessel', 'Date')],
+                           # zcol = c('Vessel', 'Date'),
+                           row.numbers = F,
+                           feature.id = F))
