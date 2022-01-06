e25510850c854b30eaab041921adc2ff39221e5e
Claire-Cecile Juhasz <ccjuhasz@campus.univ.run>
2022-01-06 13:38:11 +0400

    # Extraction of no NA and no duplicated GPS reloc data

--- PTEBAR/PTEBAR_3-Explo_duplicated_data.r
+++ PTEBAR/PTEBAR_3-Explo_duplicated_data.r
@@ -12,6 +12,8 @@ source('C:/Users/ccjuhasz/Desktop/SMAC/GITHUB/SMAC-ENTROPIE_tracking/PTEBAR/PTEB
 # Starting from the initial database
 source('C:/Users/ccjuhasz/Desktop/SMAC/GITHUB/SMAC-ENTROPIE_tracking/PTEBAR/PTEBAR_00-Corrections_initial_database.r') # database with correct date but with NA and duplicated rows
 
+summary(gps1)
+
 # Deletion of rows with NA in Longitude
 gps1_noNA <- gps1[!is.na(gps1$Longitude),]
 summary(gps1_noNA$Longitude)
@@ -23,7 +25,7 @@ gps1_noNA$LoggerTime[duplicated(gps1_noNA$LoggerTime)] # 31% of initial data is 
 
 gps_dupl_id <- unique(gps1_noNA$LoggerTime[duplicated(gps1_noNA$LoggerTime)])
 
-# Extraction of the duplicated database
+# Extraction of the duplicated database & visualization
 gps_dupl <- gps1_noNA[gps1_noNA$LoggerTime %in% gps_dupl_id,]
 
 tot <- dplyr::count(gps_dupl, gps_dupl$LoggerTime)
@@ -32,25 +34,35 @@ barplot(table(table(gps_dupl$LoggerTime)))
 gps_dupl_spat <- st_as_sf(gps_dupl,
                           coords = c('Longitude', 'Latitude'),
                           crs = 4326) # '+init=epsg:4326'
+gps_dupl_spat_UTM <- st_transform(gps_dupl_spat,
+                                  '+init=epsg:32743')
 
 mapview::mapview(gps_dupl_spat,
                  zcol = 'LoggerTime')
 
 LoggerTime <- vector(length = length(gps_dupl_id))
 max_dist <- vector(length = length(gps_dupl_id))
+max_dist_UTM <- vector(length = length(gps_dupl_id))
 n_dupl <- vector(length = length(gps_dupl_id))
 
 for(i in 1:length(gps_dupl_id)){
   
   dat <- gps_dupl_spat[gps_dupl_spat$LoggerTime == gps_dupl_id[i],]
+  dat_UTM <- gps_dupl_spat_UTM[gps_dupl_spat_UTM$LoggerTime == gps_dupl_id[i],]
   
   LoggerTime[i] <- gps_dupl_id[i]
   n_dupl[i] <- nrow(dat)
   max_dist[i] <- max(st_distance(dat))
+  max_dist_UTM[i] <- max(st_distance(dat_UTM))
 }
 
-info_dupl <- data.frame(LoggerTime, n_dupl, max_dist)
+info_dupl <- data.frame(LoggerTime, n_dupl, max_dist, max_dist_UTM)
+info_dupl
 
+# Dataframe with no NA and no duplicated data
 
+gps2 <- gps1_noNA[!duplicated(gps1_noNA$LoggerTime),]
 
+# write.table(gps2,
+            # 'C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/4-PTEBAR_GPS/DATA/PTEBAR_GPS_DB_V2_noDUPL.txt')
 
