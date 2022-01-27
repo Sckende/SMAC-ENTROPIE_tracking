97dcd3b77cad1e12242b0bfec18d165adcc4de99
Sckende <clrccljuhasz@gmail.com>
2022-01-27 18:12:44 +0400

    # data explo

--- PTEBAR-JUV/7-PTEBAR_JUV_Explo_env_data_with_relocs.r
+++ PTEBAR-JUV/7-PTEBAR_JUV_Explo_env_data_with_relocs.r
@@ -1,15 +1,57 @@
-argos <- readRDS("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/DATA/PTEBAR_JUV_argos_with_env_DATA.rds")
-summary(argos)
-class(argos$Date)
-names(argos)
-dim(argos)
+getwd()
+library(languageserver)
+library(sf)
+rm(list = ls())
 
-argos_list <- split(argos3, argos$Vessel)
-lapply(argos_list, function(x){
+################
+# Data loading #
+################
+
+
+argos_sf <- readRDS("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/DATA/PTEBAR_JUV_argos_with_env_DATA.rds")
+argos_df <- st_drop_geometry(argos_sf)
+
+
+list.files("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/DATA/")
+
+head(argos_df)
+class(argos_sf)
+class(argos_df)
+
+summary(argos_df)
+
+plot(argos_df$wind_speed[argos_df$speed.m.sec < 1000],
+     argos_df$speed.m.sec[argos_df$speed.m.sec < 1000])
+
+plot(argos_df$wind_speed[argos_df$speed.m.sec < 200],
+     argos_df$speed.m.sec[argos_df$speed.m.sec < 200])
+
+plot(argos_df$wind_speed[argos_df$speed.m.sec < 50],
+     argos_df$speed.m.sec[argos_df$speed.m.sec < 50])
+################################
+names(argos_df)
+dim(argos_df)
+
+argos_list <- split(argos_df, argos_df$Vessel)
+par(mfrow = c(3, 5))
+
+lapply(argos_list, function(x) {
     plot(x$wind_speed)
-})
+    })
 
-plot(argos_list[[1]]$wind_speed, type = 'l')
-plot(argos_list[[1]]$CHLO, col = 'darkgreen', type = 'l')
-plot(argos_list[[1]]$SST, col = 'darkorange', type = 'l')
+lapply(argos_list, function(x) {
+    plot(x$speed.m.sec,
+         type = 'l')
+    })
+
+
+
+plot(argos_env_list[[1]]$wind_speed,
+     type = 'l')
+plot(argos_env_list[[1]]$CHLO,
+     col = 'darkgreen',
+     type = 'l')
+plot(argos_env_list[[1]]$SST,
+     col = 'darkorange',
+     type = 'l')
 View()
