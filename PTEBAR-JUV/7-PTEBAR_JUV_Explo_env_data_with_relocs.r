7187b3def8968ba82277ff1ed274a2e365f5994c
Sckende <clrccljuhasz@gmail.com>
2022-02-02 16:58:19 +0400

    # Explo for wind and bird directions

--- PTEBAR-JUV/7-PTEBAR_JUV_Explo_env_data_with_relocs.r
+++ PTEBAR-JUV/7-PTEBAR_JUV_Explo_env_data_with_relocs.r
@@ -54,12 +54,14 @@ u_2017 <- zon_stack[[which(names(zon_stack) %in% period_1)]]
 names(u_2017)
 hist(values(u_2017[[1]]))
 mean_u_2017 <- mean(u_2017)
+mean_u_2017
 
 # ----- #
 v_2017 <- mer_stack[[which(names(mer_stack) %in% period_1)]]
 names(v_2017)
 hist(values(v_2017[[1]]))
 mean_v_2017 <- mean(v_2017)
+mean_v_2017
 
 # ----- #
 x11()
@@ -80,6 +82,7 @@ u_2018 <- dropLayer(zon_stack,
 names(u_2018)
 
 mean_u_2018 <- mean(u_2018)
+mean_u_2018
 
 # ----- #
 v_2018 <- dropLayer(mer_stack,
@@ -88,6 +91,7 @@ v_2018 <- dropLayer(mer_stack,
 names(v_2018)
 
 mean_v_2018 <- mean(v_2018)
+mean_v_2018
 
 # ----- #
 x11()
@@ -188,68 +192,116 @@ for(i in t_2018){
          key.footer = "WSP (m/s)",
          key.position = "bottom",
          par.settings = list(axis.line = list(col = "lightgray")),
-          col = viridis(5, option = "A"))
+          col = viridis(5,
+                        option = "A",
+                        begin = 1,
+                        end = 0))
 
 }
 
-# summary(wind)
-# x11()
-# hist(wind$wd0_360)
-# x11()
-# hist(wind$wd1)
-
-# x11()
-
-# From 0 to 360
-x11()
-windRose(mydata = wind,
-         wd = "wd2",
-         ws = "ws",
-         breaks = c(0, 2, 5, 8, 11, 17),
-         auto.text = F,
-         paddle = F,
-         annotate = T,
-         grid.line = 5,
-     #     key = list(labels = c(">0 - 2",
-     #                           ">2 - 5",
-     #                           ">5 - 8",
-     #                           ">8 - 11",
-     #                           "> 11")),
-         key.footer = "WSP (m/s)",
-         key.position = "bottom",
-         par.settings = list(axis.line = list(col = "lightgray")),
-         col = viridis(5))
 
 # Next step - Work with the type argument of the function
-dim(wind)
-wind$type <- rep(1:15, 13363)
-wind$type <- as.factor(wind$type)
 
+
+#####################################################
+#### Bird direction & wind direction exploration ####
+#####################################################
+g <- readRDS("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/DATA/RMD/PTEBAR_JUV_argos_&_speed_ltraj.rds")
+g
+str(g)
+g2 <- do.call("rbind", g)
+summary(g2$abs.angle)
+summary(g2$rel.angle)
+
+tt <- g[[3]] # angles in radians to verify
+dim(tt)
+names(tt)
+class(tt)
+
+plot(tt$x,
+     tt$y,
+     type = "b")
+
+test <- tt
+plot(test$x,
+     test$y,
+     type = "b")
+
+# Working on the absolute angle alpha
+# Conversion from radian to degree
+test$abs.angle_deg <- test$abs.angle * 180/pi
+summary(test$abs.angle_deg)
+
+# Conversion from -180/180 to 0/360 system
+test$wd0_360 <- ifelse(test$abs.angle_deg >= 0,
+                       test$abs.angle_deg,
+                       360 + test$abs.angle_deg) # handmade conversion from -180/180 to 0/360 for windRose() plot
+
+hist(test$wd0_360[test$wd0_360 > 180])
+hist(test$abs.angle_deg[test$abs.angle_deg < 0])
+
+# Conversion from math angle convention to METEOROLOGICAL angle convention
+# https://math.stackexchange.com/questions/1589793/a-formula-to-convert-a-counter-clockwise-angle-to-clockwise-angle-with-an-offset
+
+############################################
+#### Direction que prennent les oiseaux ####
+############################################
+
+test$wd0_360_METEO_TOWARD <- (-1 * test$wd0_360 + 90) %% 360 # Direction que prennent les oiseaux
+
+############################################
+#### Direction d'où portent les oiseaux ####
+############################################
+
+test$wd0_360_METEO_FROM <- ifelse(test$wd0_360_METEO <= 180,
+                               test$wd0_360_METEO + 180,
+                               test$wd0_360_METEO - 180) # Direction d'où partent les oiseaux
+summary(test$wd0_360_METEO_FROM)
+
+# test$speed.m.s <- 1
+test$abs.speed <- sqrt(test$dx^2 + test$dy^2)
 x11()
-windRose(mydata = wind,
-         wd = "wd0_360",
-         ws = "ws",
-         type = "type",
+windRose(mydata = test, # meteorological wind projection with N = 0 = 360, E = 90, S = 180, W = 270 - Necessity to have angle from 0 to 360
+         wd = "wd0_360_METEO_TOWARD",
+         ws = "speed.m.s",
          breaks = c(0, 2, 5, 8, 11, 17),
+         angle = 20,
          auto.text = F,
          paddle = F,
-         annotate = T,
+         annotate = F,
          grid.line = 5,
-     #     key = list(labels = c(">0 - 2",
-     #                           ">2 - 5",
-     #                           ">5 - 8",
-     #                           ">8 - 11",
-     #                           "> 11")),
          key.footer = "WSP (m/s)",
          key.position = "bottom",
          par.settings = list(axis.line = list(col = "lightgray")),
-         col = viridis(5))
+          col = viridis(5, option = "D"))
+x11()
+plot(test$x,
+     test$y,
+     type = "b")
 
+test
+
+test$dir <- 180 * atan2(test$dy, test$dx)/ pi
+head(test)
+
+
+
+# Conversion for fitting on METEOROLOGICAL convention
+# N = 0 = 360, E = 90, S = 180, W = 270
+# math direction = 270 - wind direction
+# wind direction = 270 - math direction
+test$abs.angle_deg_meteo <- 270 - test$abs.angle_deg
+
+
+x11()
+windRose(mydata = test,
+        wd = "wd0_360",
+        ws = "speed.m.s")
 
 ##############################################
 #### Bird speed vs wind speed exploration ####
 ##############################################
-
+names(argos_df)
 plot(argos_df$wind_speed[argos_df$speed.m.sec < 1000],
      argos_df$speed.m.sec[argos_df$speed.m.sec < 1000])
 
