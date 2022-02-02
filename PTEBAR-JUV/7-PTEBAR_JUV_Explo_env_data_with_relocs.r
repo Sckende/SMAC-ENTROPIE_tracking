832dd87fbce243b1aaad3c15e1a8099df5f0c653
Sckende <clrccljuhasz@gmail.com>
2022-02-02 17:40:39 +0400

    # Script cleaning

--- PTEBAR-JUV/7-PTEBAR_JUV_Explo_env_data_with_relocs.r
+++ PTEBAR-JUV/7-PTEBAR_JUV_Explo_env_data_with_relocs.r
@@ -53,15 +53,11 @@ period_1 <- names(zon_stack)[str_which(names(zon_stack), "X2017.")]
 u_2017 <- zon_stack[[which(names(zon_stack) %in% period_1)]]
 names(u_2017)
 hist(values(u_2017[[1]]))
-mean_u_2017 <- mean(u_2017)
-mean_u_2017
 
 # ----- #
 v_2017 <- mer_stack[[which(names(mer_stack) %in% period_1)]]
 names(v_2017)
 hist(values(v_2017[[1]]))
-mean_v_2017 <- mean(v_2017)
-mean_v_2017
 
 # ----- #
 x11()
@@ -113,10 +109,17 @@ vectorplot(raster::stack(mean_u_2018, mean_v_2018),
 # YEAR 2017 ####
 ################
 
-dirs_2017 <- 180 * atan2(v_2017, u_2017) / pi # atan2 gives direction in radian, then *180/pi allows the conversion in degree from -180 to 180
-# In addition, atan2 gives the angle with METEOROLOGICAL convention
-# N = 0 = 360, E = 90, S = 180, W = 270
+# dirs_2017 <- 180 * atan2(v_2017, u_2017) / pi # atan2 gives direction in radian, then *180/pi allows the conversion in degree from -180 to 180
+# # In addition, atan2 gives the angle with METEOROLOGICAL convention
+# # N = 0 = 360, E = 90, S = 180, W = 270
+# saveRDS(dirs_2017,
+#         "C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/ENV_DATA_Romain/Pre_treat/wind_dirs_2017.rds")
+
+dirs_2017 <- readRDS("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/ENV_DATA_Romain/Pre_treat/wind_dirs_2017.rds")
 dirs_2017 # From -180 to 180
+
+# -----> absolute wind speed from u & v components
+##################################################
 
 abs_wind_sp_2017 <- sqrt(u_2017^2 + v_2017^2)
 abs_wind_sp_2017
@@ -124,17 +127,25 @@ abs_wind_sp_2017
 # YEAR 2018 ####
 ################
 
-dirs_2018 <- 180 * atan2(v_2018, u_2018) / pi # atan2 gives direction in radian, then *180/pi allows the conversion in degree from -180 to 180
-# In addition, atan2 gives the angle with METEOROLOGICAL convention
+# dirs_2018 <- 180 * atan2(v_2018, u_2018) / pi # atan2 gives direction in radian, then *180/pi allows the conversion in degree from -180 to 180
+# In addition, HERE the atan2 gives the angle with METEOROLOGICAL convention
 # N = 0 = 360, E = 90, S = 180, W = 270
+# saveRDS(dirs_2018,
+#         "C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/ENV_DATA_Romain/Pre_treat/wind_dirs_2018.rds")
+
+dirs_2018 <- readRDS("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/ENV_DATA_Romain/Pre_treat/wind_dirs_2018.rds")
 dirs_2018 # From -180 to 180
+
+# -----> absolute wind speed fromm u & v components
+###################################################
 
 abs_wind_sp_2018 <- sqrt(u_2018^2 + v_2018^2)
 abs_wind_sp_2018
 
-#############################################
-# ---- > Visualization of wind direction ####
-#############################################
+
+###########################################
+# ---- > Wind roses of wind directions ####
+###########################################
 
 t_2017 <- sample(1:744, 15) # random sampling of 15 layers
 t_2018 <- sample(1:744, 15) # random sampling of 15 layers
@@ -202,18 +213,18 @@ for(i in t_2018){
 
 # Next step - Work with the type argument of the function
 
+###########################################
+# ---- > Wind roses of bird directions ####
+###########################################
 
-#####################################################
-#### Bird direction & wind direction exploration ####
-#####################################################
 g <- readRDS("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/DATA/RMD/PTEBAR_JUV_argos_&_speed_ltraj.rds")
 g
 str(g)
 g2 <- do.call("rbind", g)
-summary(g2$abs.angle)
+summary(g2$abs.angle) # angles in radians
 summary(g2$rel.angle)
 
-tt <- g[[3]] # angles in radians to verify
+tt <- g[[3]] 
 dim(tt)
 names(tt)
 class(tt)
@@ -228,11 +239,14 @@ plot(test$x,
      type = "b")
 
 # Working on the absolute angle alpha
-# Conversion from radian to degree
-test$abs.angle_deg <- test$abs.angle * 180/pi
+#####################################
+# Which corresponds to 180 * atan2(dx, dy) / pi
+
+# -----> Conversion from radian to degree
+test$abs.angle_deg <- test$abs.angle * 180/pi # angles from -180 to +180 in mathematical convention
 summary(test$abs.angle_deg)
 
-# Conversion from -180/180 to 0/360 system
+# ----- > Conversion from -180/180 to 0/360 system
 test$wd0_360 <- ifelse(test$abs.angle_deg >= 0,
                        test$abs.angle_deg,
                        360 + test$abs.angle_deg) # handmade conversion from -180/180 to 0/360 for windRose() plot
@@ -240,26 +254,24 @@ test$wd0_360 <- ifelse(test$abs.angle_deg >= 0,
 hist(test$wd0_360[test$wd0_360 > 180])
 hist(test$abs.angle_deg[test$abs.angle_deg < 0])
 
-# Conversion from math angle convention to METEOROLOGICAL angle convention
+# -----> Conversion from math angle convention to METEOROLOGICAL angle convention
 # https://math.stackexchange.com/questions/1589793/a-formula-to-convert-a-counter-clockwise-angle-to-clockwise-angle-with-an-offset
 
-############################################
-#### Direction que prennent les oiseaux ####
-############################################
+# ----------------> TOWARD DIRECTION 
 
 test$wd0_360_METEO_TOWARD <- (-1 * test$wd0_360 + 90) %% 360 # Direction que prennent les oiseaux
 
-############################################
-#### Direction d'où portent les oiseaux ####
-############################################
+# ----------------> FROM WHICH DIRECTION
 
 test$wd0_360_METEO_FROM <- ifelse(test$wd0_360_METEO <= 180,
                                test$wd0_360_METEO + 180,
                                test$wd0_360_METEO - 180) # Direction d'où partent les oiseaux
 summary(test$wd0_360_METEO_FROM)
 
-# test$speed.m.s <- 1
+# -----> Calculating the absolute wind speed from dy & dx 
 test$abs.speed <- sqrt(test$dx^2 + test$dy^2)
+
+# -----> Wind roses
 x11()
 windRose(mydata = test, # meteorological wind projection with N = 0 = 360, E = 90, S = 180, W = 270 - Necessity to have angle from 0 to 360
          wd = "wd0_360_METEO_TOWARD",
@@ -279,63 +291,8 @@ plot(test$x,
      test$y,
      type = "b")
 
-test
+test$burst <- substring(row.names(test), 7, 10)
 
-test$dir <- 180 * atan2(test$dy, test$dx)/ pi
-head(test)
-
-
-
-# Conversion for fitting on METEOROLOGICAL convention
-# N = 0 = 360, E = 90, S = 180, W = 270
-# math direction = 270 - wind direction
-# wind direction = 270 - math direction
-test$abs.angle_deg_meteo <- 270 - test$abs.angle_deg
-
-
-x11()
-windRose(mydata = test,
-        wd = "wd0_360",
-        ws = "speed.m.s")
-
-##############################################
-#### Bird speed vs wind speed exploration ####
-##############################################
-names(argos_df)
-plot(argos_df$wind_speed[argos_df$speed.m.sec < 1000],
-     argos_df$speed.m.sec[argos_df$speed.m.sec < 1000])
-
-plot(argos_df$wind_speed[argos_df$speed.m.sec < 200],
-     argos_df$speed.m.sec[argos_df$speed.m.sec < 200])
-
-plot(argos_df$wind_speed[argos_df$speed.m.sec < 50],
-     argos_df$speed.m.sec[argos_df$speed.m.sec < 50])
-
-################################
-
-names(argos_df)
-dim(argos_df)
-
-argos_list <- split(argos_df, argos_df$Vessel)
-par(mfrow = c(3, 5))
-
-lapply(argos_list, function(x) {
-    plot(x$wind_speed)
-    })
-
-lapply(argos_list, function(x) {
-    plot(x$speed.m.sec,
-         type = 'l')
-    })
-
-
-par(mfrow = c(3, 1))
-plot(argos_list[[1]]$wind_speed,
-     type = 'l')
-plot(argos_list[[1]]$CHLO,
-     col = 'darkgreen',
-     type = 'l')
-plot(argos_list[[1]]$SST,
-     col = 'darkorange',
-     type = 'l')
-View()
+# NEXT STEP, split in range of time
+# Extract wind direction for each loc of birds
+# Extract bathymetrie for each loc of birds
