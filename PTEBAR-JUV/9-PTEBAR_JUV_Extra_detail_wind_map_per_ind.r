ce8e6b9ab3340576db1285b38060a81ca9717017
Sckende <clrccljuhasz@gmail.com>
2022-03-01 15:44:17 +0400

    # Detailled tracks 2017 - part 1 - completed

--- PTEBAR-JUV/9-PTEBAR_JUV_Extra_detail_wind_map_per_ind.r
+++ PTEBAR-JUV/9-PTEBAR_JUV_Extra_detail_wind_map_per_ind.r
@@ -13,11 +13,16 @@ library(dplyr)
 library(magick)
 library(latticeExtra)
 library(mapview)
+library(officer)
 
 zon <- readRDS("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/ENV_DATA_Romain/Pre_treat/wind_north_stack.rds")
+zon <- zon[[order(names(zon))]]
+
 mer <- readRDS("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/ENV_DATA_Romain/Pre_treat/wind_east_stack.rds")
+mer <- mer[[order(names(mer))]]
 
 speed <- readRDS("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/ENV_DATA_Romain/Pre_treat/wind_speed_stack.rds")
+speed <- speed[[order(names(speed))]]
 
 argos <- readRDS("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/DATA/PTEBAR_JUV_argos_with_env_DATA_wind_dirs_n_abs_speed_2.rds")
 argos_sp <- SpatialPointsDataFrame(coords = argos[, c("Longitude", "Latitude")],
@@ -57,12 +62,12 @@ dim(argos_2017_sp)
 # -----> Typical tracks
 #######################
 div_2017_566 <- argos_2017_sp[argos_2017_sp$Vessel == "166566", ]
-div_2017_568 <- argos_2017_sp[argos_2017_sp$Vessel == "166568", ]
+div_2017_568 <- argos_2017_sp[argos_2017_sp$Vessel == "166568", ] # the longest
 
 # -----> Atypical tracks
 ########################
 div_2017_569 <- argos_2017_sp[argos_2017_sp$Vessel == "166569", ]
-div_2017_572 <- argos_2017_sp[argos_2017_sp$Vessel == "166572", ]
+div_2017_572 <- argos_2017_sp[argos_2017_sp$Vessel == "166572", ] # the longest
 
 mapview(div_2017_566, col.regions = "#0863eb") +
     mapview(div_2017_568, col.regions = "#0b728b") +
@@ -76,6 +81,7 @@ mapview(div_2017_566, col.regions = "#0863eb") +
 # From 1 April 2017 to 9 May 2017
 
 # -----> Subset of argos data
+#############################
 
 # <----- Device 166568 ----->
 div_2017_568_crop1 <- div_2017_568[div_2017_568$point.group <= 19, ]
@@ -84,65 +90,70 @@ summary(div_2017_568_crop1$Date)
 # <----- Device 166566 ----->
 div_2017_566_crop1 <- div_2017_566
 summary(div_2017_566_crop1$Date)
+# ==> min date = 2017-04-06
+# ==> max date = 2017-05-09
 
-# -----> Subset of raster layers - CROP 1
+# -----> Temporal selection of layers - CROP 1
+###############################################
+# Number of first layer
 raster_min_2017_crop1 <- 1
-# ----- #
-div_2017_566_crop1$raster_layer
-layer <- paste(substr(div_2017_566_crop1$raster_layer[93], 1, 11),
-               "18.00",
-               sep = "") # Pour avoir les toutes les couches de lma dernière date
+# Number of last layer based on the maximal date - 2017-05-09
 raster_max_2017_crop1 <- str_which(names(zon),
-                                   layer)
-
-
+                                   "2017.05.09.18.00")
+# Selection of layers
 zon_1 <- zon[[raster_min_2017_crop1:raster_max_2017_crop1]]
 mer_1 <- mer[[raster_min_2017_crop1:raster_max_2017_crop1]]
 spe_1 <- speed[[raster_min_2017_crop1:raster_max_2017_crop1]]
 
-# -----> Cropping of raster layers - CROP 1
-ext_2017_1 <- extent(40, 70, -35, 0)
-z_2017_1 <- crop(x = zon_1,
-                 y = ext_2017_1)
-m_2017_1 <- crop(x = mer_1,
-                 y = ext_2017_1)
-s_2017_1 <- crop(x = spe_1,
-                 y = ext_2017_1)
+# -----> Spatial selection of layers - CROP 1
+##############################################
+ext_2017_crop1 <- extent(40, 60, -30, -10) # xmin, xmax, ymin, ymax
+z_2017_crop1 <- crop(x = zon_1,
+                     y = ext_2017_crop1)
+m_2017_crop1 <- crop(x = mer_1,
+                     y = ext_2017_crop1)
+s_2017_crop1 <- crop(x = spe_1,
+                     y = ext_2017_crop1)
 
 # -----> CROP 2 ####
 ####################
-# From 10 May to the end
-div_2017_568_crop2 <- div_2017_568[div_2017_568$point.group > 19, ]
+# From 10 May 2017 to the end
+div_2017_568_crop2 <- div_2017_568[div_2017_568$point.group >= 20, ]
 dim(div_2017_568_crop2)
 summary(div_2017_568_crop2$Date)
 
-# -----> Subset of raster layers - CROP 2
-raster_min_2017_crop1 <- raster_max_2017_crop1 + 1
-raster_max_2017_crop1 <- nlayers(z_2017_1)
+# ==> min date = 2017-05-09 16:57:00
+# ==> max date = 2017-05-22 16:00:00
 
+# -----> Temporal selection of layers - CROP 2
+##############################################
+# Number of first layer based on the minimal date - 2017-05-09
+raster_min_2017_crop2 <- str_which(names(zon),
+                                   "2017.05.09.00.00")
+# Number of last layer based on the maximal date - 2017-05-22
+raster_max_2017_crop2 <- str_which(names(zon),
+                                   "2017.05.22.18.00")
+# Selection of layers
+zon_2 <- zon[[raster_min_2017_crop2:raster_max_2017_crop2]]
+mer_2 <- mer[[raster_min_2017_crop2:raster_max_2017_crop2]]
+spe_2 <- speed[[raster_min_2017_crop2:raster_max_2017_crop2]]
 
-# zon_1 <- zon[[str_which(names(zon), raster_min_c1):
-#               str_which(names(zon), raster_max_c1)]]
-# mer_1 <- mer[[str_which(names(mer), raster_min_c1):
-#               str_which(names(mer), raster_max_c1)]]
-# spe_1 <- speed[[str_which(names(speed), raster_min_c1):
-#               str_which(names(speed), raster_max_c1)]]
-
-# # -----> Cropping of raster layers - CROP 1
-# ext_2017_1 <- extent(40, 70, -35, 0)
-# z_2017_1 <- crop(x = zon_1,
-#                  y = ext_2017_1)
-# m_2017_1 <- crop(x = mer_1,
-#                  y = ext_2017_1)
-# s_2017_1 <- crop(x = spe_1,
-#                  y = ext_2017_1)
+# -----> Spatial selection of layers - CROP 2
+##############################################
+ext_2017_crop2 <- extent(35, 55, -15, 5)
+z_2017_crop2 <- crop(x = zon_2,
+                     y = ext_2017_crop2)
+m_2017_crop2 <- crop(x = mer_2,
+                     y = ext_2017_crop2)
+s_2017_crop2 <- crop(x = spe_2,
+                     y = ext_2017_crop2)
 
 # -----> 2017 - 166568 - crop 1 ####
 ####################################
-for(i in 1:nlayers(z_2017_1)){
+for (i in 1:nlayers(z_2017_crop1)) {
     png(paste("C:/Users/ccjuhasz/Desktop/Meeting_H_Weimerskirch/MAPS/FAKIR_6hours/2017-166568/crop1/",
-                "2017-166568-",
-                names(new_z_1[[i]]),
+                "CROP1-2017-166568-",
+                names(z_2017_crop1[[i]]),
               ".png",
               sep = ""),
     res = 300,
@@ -152,45 +163,44 @@ for(i in 1:nlayers(z_2017_1)){
     unit = "cm",
     bg = "transparent")
 
-    dat <- substr(names(z_2017_1[[i]]), 2, 17)
-    locs568 <- div_2017_568_crop1[div_2017_568_crop1$raster_layer == dat,]
-    back_locs_568 <- div_2017_568_crop1[div_2017_568_crop1$Date < as.POSIXlt(dat,
-                                                                             format = "%Y.%m.%d"),]
+    dat <- substr(names(z_2017_crop1[[i]]), 2, 17)
+    locs568 <- div_2017_568_crop1[div_2017_568_crop1$raster_layer == dat, ]
+    dat_conv <- as.POSIXlt(dat,
+                           format = "%Y.%m.%d.%H.%M")
+    back_locs_568 <- div_2017_568_crop1[div_2017_568_crop1$Date < dat_conv, ]
 
     # x11()
     print(
-    vectorplot(stack(m_2017_1[[i]], z_2017_1[[i]]),
+    vectorplot(stack(m_2017_crop1[[i]], z_2017_crop1[[i]]),
                   isField = 'dXY',
-                  region =  s_2017_1[[i]],
+                  region =  s_2017_crop1[[i]],
                   at = my_at,
                   lwd.arrows = 1,
                   aspX = 0.2,
                   narrows = 300,
                   col.regions = my_cols,
                   main = paste("2017 - TYPICAL - CROP 1 - ",
-                               names(m_2017_1[[i]]))) +
+                               names(m_2017_crop1[[i]]))) +
     layer(c(sp.points(back_locs_568,
                       col = "#d7f3b7",
                       lwd = 3,
                       cex = 2),
             sp.points(locs568,
                       col = "#4a8a02",
-                      lwd = 3,
-                      cex = 2,
-                      cex.lab = 2)))
+                      lwd = 6,
+                      cex = 4)))
     )
 
 dev.off()
-print(i)
+print(paste(i, "/", nlayers(z_2017_crop1), sep = ""))
 }
 
-# -----> 2017 - 166566 - crop 1 ####
+# -----> 2017 - 166568 - crop 2 ####
 ####################################
-
-for(i in 1:nlayers(z_2017_1)){
-    png(paste("C:/Users/ccjuhasz/Desktop/Meeting_H_Weimerskirch/MAPS/FAKIR_6hours/2017-166566/crop1/",
-                "2017-166566-",
-                names(new_z_1[[i]]),
+for (i in 1:nlayers(z_2017_crop2)) {
+    png(paste("C:/Users/ccjuhasz/Desktop/Meeting_H_Weimerskirch/MAPS/FAKIR_6hours/2017-166568/crop2/",
+                "CROP2-2017-166568-",
+                names(z_2017_crop2[[i]]),
               ".png",
               sep = ""),
     res = 300,
@@ -200,33 +210,151 @@ for(i in 1:nlayers(z_2017_1)){
     unit = "cm",
     bg = "transparent")
 
-    dat <- substr(names(z_2017_1[[i]]), 2, 17)
-    locs566 <- div_2017_566_crop1[div_2017_566_crop1$raster_layer == dat,]
-    back_locs_566 <- div_2017_566_crop1[div_2017_566_crop1$Date < as.POSIXlt(dat,
-                                                                             format = "%Y.%m.%d"),]
+    dat <- substr(names(z_2017_crop2[[i]]), 2, 17)
+    locs568 <- div_2017_568_crop2[div_2017_568_crop2$raster_layer == dat, ]
+    dat_conv <- as.POSIXlt(dat,
+                           format = "%Y.%m.%d.%H.%M")
+    back_locs_568 <- div_2017_568_crop2[div_2017_568_crop2$Date < dat_conv, ]
+
     # x11()
     print(
-    vectorplot(stack(m_2017_1[[i]], z_2017_1[[i]]),
+    vectorplot(stack(m_2017_crop2[[i]], z_2017_crop2[[i]]),
                   isField = 'dXY',
-                  region =  s_2017_1[[i]],
+                  region =  s_2017_crop2[[i]],
+                  at = my_at,
+                  lwd.arrows = 1,
+                  aspX = 0.2,
+                  narrows = 300,
+                  col.regions = my_cols,
+                  main = paste("2017 - TYPICAL - CROP 2 - ",
+                               names(m_2017_crop2[[i]]))) +
+    layer(c(sp.points(back_locs_568,
+                      col = "#caf599",
+                      lwd = 3,
+                      cex = 2),
+            sp.points(locs568,
+                      col = "#5aa505",
+                      lwd = 6,
+                      cex = 4)))
+    )
+
+dev.off()
+print(paste(i, "/", nlayers(z_2017_crop2), sep = ""))
+}
+
+# -----> 2017 - 166566 - crop 1 ####
+####################################
+
+for (i in 1:nlayers(z_2017_crop1)) {
+    png(paste("C:/Users/ccjuhasz/Desktop/Meeting_H_Weimerskirch/MAPS/FAKIR_6hours/2017-166566/crop1/",
+                "CROP1-2017-166566-",
+                names(z_2017_crop1[[i]]),
+              ".png",
+              sep = ""),
+    res = 300,
+    width = 40,
+    height = 50,
+    pointsize = 12,
+    unit = "cm",
+    bg = "transparent")
+
+    dat <- substr(names(z_2017_crop1[[i]]), 2, 17)
+    locs566 <- div_2017_566_crop1[div_2017_566_crop1$raster_layer == dat, ]
+    dat_conv <- as.POSIXlt(dat,
+                           format = "%Y.%m.%d.%H.%M")
+    back_locs_566 <- div_2017_566_crop1[div_2017_566_crop1$Date < dat_conv, ]
+    # x11()
+    print(
+    vectorplot(stack(m_2017_crop1[[i]], z_2017_crop1[[i]]),
+                  isField = 'dXY',
+                  region =  s_2017_crop1[[i]],
                   at = my_at,
                   lwd.arrows = 1,
                   aspX = 0.2,
                   narrows = 300,
                   col.regions = my_cols,
                   main = paste("2017 - TYPICAL - CROP 1 - ",
-                               names(m_2017_1[[i]]))) +
+                               names(m_2017_crop1[[i]]))) +
     layer(c(sp.points(back_locs_566,
                       col = "#aec1f5",
                       lwd = 3,
                       cex = 2),
             sp.points(locs566,
                       col = "#0042f8",
-                      lwd = 3,
-                      cex = 2)))
+                      lwd = 6,
+                      cex = 4)))
 # )
     )
 
 dev.off()
-print(i)
+print(paste(i, "/", nlayers(z_2017_crop1), sep = ""))
 }
+
+# -----> PPT creation ####
+##########################
+
+# File list
+files_2017_568 <- list.files("C:/Users/ccjuhasz/Desktop/Meeting_H_Weimerskirch/MAPS/FAKIR_6hours/2017-166568/")
+length(files_2017_568)
+
+files_2017_566 <- list.files("C:/Users/ccjuhasz/Desktop/Meeting_H_Weimerskirch/MAPS/FAKIR_6hours/2017-166566/")
+length(files_2017_566)
+
+# Doc creation
+doc <- read_pptx()
+doc <- add_slide(doc,
+                 layout = "Two Content",
+                 master = "Office Theme")
+
+for (i in 1:length(files_2017_568)) {
+    
+    # file path
+    img_file_568 <- paste("C:/Users/ccjuhasz/Desktop/Meeting_H_Weimerskirch/MAPS/FAKIR_6hours/2017-166568/",
+                          files_2017_568[i],
+                          sep = "")
+    if (!is.na(files_2017_566[i])) {
+        img_file_566 <- paste("C:/Users/ccjuhasz/Desktop/Meeting_H_Weimerskirch/MAPS/FAKIR_6hours/2017-166566/",
+                              files_2017_566[i],
+                              sep = "")
+        doc <- ph_with(x = doc,
+                       value = paste("Typical tracks in 2017 - ",
+                       substr(files_2017_568[i],
+                                      20,
+                                      35),
+                       sep = ""),
+                       location = ph_location_type(type = "title"))
+        doc <- ph_with(x = doc,
+                       value = "166568",
+                       location = ph_location_left())
+        doc <- ph_with(x = doc,
+                       value = "166566",
+                       location = ph_location_right())
+        doc <- ph_with(x = doc,
+                       value = external_img(img_file_568),
+                       location = ph_location_left(),
+                       use_loc_size = TRUE)
+        doc <- ph_with(x = doc,
+                       value = external_img(img_file_566),
+                       location = ph_location_right(),
+                       use_loc_size = TRUE)
+        doc <- add_slide(doc)
+    } else {
+                doc <- ph_with(x = doc,
+                       value = substr(files_2017_568[i],
+                                      20,
+                                      35),
+                       location = ph_location_type(type = "title"))
+        doc <- ph_with(x = doc,
+                       value = "166568",
+                       location = ph_location_left())
+        doc <- ph_with(x = doc,
+                       value = external_img(img_file_568),
+                       location = ph_location_left(),
+                       use_loc_size = TRUE)
+        doc <- add_slide(doc)
+    }
+    print(i)
+}
+
+print(doc,
+      target = "C:/Users/ccjuhasz/Desktop/Meeting_H_Weimerskirch/2017_TYPICAL_TRACKS.pptx")
