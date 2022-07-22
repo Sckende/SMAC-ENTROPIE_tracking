5bd9248254cdede668903b41adc9b61e675475c2
Sckende <clrccljuhasz@gmail.com>
2022-07-22 11:44:54 +0400

    # map production

--- PTEBAR-JUV/ENVIRON_variables_WIND_Carto_data_mensuelle_2008-2018.r
+++ PTEBAR-JUV/ENVIRON_variables_WIND_Carto_data_mensuelle_2008-2018.r
@@ -54,52 +54,52 @@ names(speed2[[names(speed2)[month(time(speed2)) == 8]]])
 # Mean values for speed
 # ----- #
  
-for(i in 1:12) {
+# for(i in 1:12) {
     
-    mean_rast <- mean(speed2[[names(speed2)[month(time(speed2)) == i]]])
+#     mean_rast <- mean(speed2[[names(speed2)[month(time(speed2)) == i]]])
     
-    terra::writeRaster(mean_rast,
-                       paste("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/ENV_DATA_Romain/Pre_treat/Monthly_wind_2008-2018/mean_wind_speed_month_",
-                             i,
-                             ".tif",
-                             sep = ""),
-                       overwrite = T)
-    print(paste(i, " is done", sep = ""))
-}
+#     terra::writeRaster(mean_rast,
+#                        paste("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/ENV_DATA_Romain/Pre_treat/Monthly_wind_2008-2018/mean_wind_speed_month_",
+#                              i,
+#                              ".tif",
+#                              sep = ""),
+#                        overwrite = T)
+#     print(paste(i, " is done", sep = ""))
+# }
 
 # ----- #
 # Mean values for east
 # ----- #
 
-for(i in 1:12) {
+# for(i in 1:12) {
     
-    mean_rast <- mean(east2[[names(east2)[month(time(east2)) == i]]])
+#     mean_rast <- mean(east2[[names(east2)[month(time(east2)) == i]]])
     
-    terra::writeRaster(mean_rast,
-                       paste("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/ENV_DATA_Romain/Pre_treat/Monthly_wind_2008-2018/mean_wind_east_month_",
-                             i,
-                             ".tif",
-                             sep = ""),
-                       overwrite = T)
-    print(paste(i, " is done", sep = ""))
-}
+#     terra::writeRaster(mean_rast,
+#                        paste("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/ENV_DATA_Romain/Pre_treat/Monthly_wind_2008-2018/mean_wind_east_month_",
+#                              i,
+#                              ".tif",
+#                              sep = ""),
+#                        overwrite = T)
+#     print(paste(i, " is done", sep = ""))
+# }
 
 # ----- #
 # Mean values for north
 # ----- #
 
-for(i in 1:12) {
+# for(i in 1:12) {
     
-    mean_rast <- mean(north2[[names(north2)[month(time(north2)) == i]]])
+#     mean_rast <- mean(north2[[names(north2)[month(time(north2)) == i]]])
     
-    terra::writeRaster(mean_rast,
-                       paste("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/ENV_DATA_Romain/Pre_treat/Monthly_wind_2008-2018/mean_wind_north_month_",
-                             i,
-                             ".tif",
-                             sep = ""),
-                       overwrite = T)
-    print(paste(i, " is done", sep = ""))
-}
+#     terra::writeRaster(mean_rast,
+#                        paste("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/ENV_DATA_Romain/Pre_treat/Monthly_wind_2008-2018/mean_wind_north_month_",
+#                              i,
+#                              ".tif",
+#                              sep = ""),
+#                        overwrite = T)
+#     print(paste(i, " is done", sep = ""))
+# }
 # ----- #
 # Stack des valeurs mensuelles par parametres 
 # ----- #
@@ -107,24 +107,125 @@ for(i in 1:12) {
 m_speed <- list.files("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/ENV_DATA_Romain/Pre_treat/Monthly_wind_2008-2018/",
                    pattern = "mean_wind_speed_month",
                    full.names = TRUE)
-mean_speed <- terra::rast(m_speed)
+mean_speed <- terra::rast(m_speed) # SpatRaster
+ms <- raster::stack(m_speed) # RasterStack
 
 x11();plot(mean_speed)
+x11();plot(ms)
 
 # ----- #
 
 m_east <- list.files("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/ENV_DATA_Romain/Pre_treat/Monthly_wind_2008-2018/",
                    pattern = "mean_wind_east_month",
                    full.names = TRUE)
-mean_east <- terra::rast(m_east)
+mean_east <- terra::rast(m_east) # SpatRaster
+me <- raster::stack(m_east) # RasterStack
 
 x11();plot(mean_east)
+x11(); plot(me)
 
 # ----- #
 
 m_north <- list.files("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/ENV_DATA_Romain/Pre_treat/Monthly_wind_2008-2018/",
                    pattern = "mean_wind_north_month",
                    full.names = TRUE)
-mean_north <- terra::rast(m_north)
+mean_north <- terra::rast(m_north) # SpatRaster
+mn <- raster::stack(m_north) # RasterStack
 
 x11();plot(mean_north)
+x11(); plot(mn)
+
+# zonal = x = east component
+# meridional = y = north component
+nlev <- 100
+my_at <- seq(from = 0,
+             to = 20,
+             length.out = nlev + 1)
+my_cols <- viridis_pal(begin = 1,
+                       end = 0,
+                       option = "A")(nlev)
+for (i in 1:12) {
+    
+#    x11()
+png(paste("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/X-PTEBAR_argos_JUV/PTEBAR_JUV_Carto/WIND_2008-2018/",
+          i,
+          "-WIND_mean_2008-2018_",
+          month.name[i],
+          ".png",
+          sep = ""),
+    res = 300,
+    width = 50,
+    height = 40,
+    pointsize = 20,
+    unit = "cm",
+    # bg = "transparent",
+    bg = "white")
+
+   print(vectorplot(raster::stack(me[[i]], mn[[i]]),
+                 narrows = 800,
+                 aspX = 0.4,
+                 isField = 'dXY',
+                 region = ms[[i]],
+                 at = my_at,
+                 col.regions = my_cols,
+                 lwd.arrows = 1,
+                 colorkey = list(labels = list(cex = 2)),
+                 main = list(paste(month.name[i], " - Mean value", sep = ""),
+                             cex = 2.5),
+                 xlab = list("Longitude",
+                             cex = 2.5),
+                 ylab = list("Latitude",
+                             cex = 2.5))) 
+   dev.off()
+}
+
+graphics.off()
+
+mean_east[[1]]
+levelplot(mean_east)
+vectorplot(mean_east)
+
+
+
+
+
+
+
+
+
+
+
+# nlev <- 100
+# my_at <- seq(from = 0,
+#              to = 20,
+#              length.out = nlev + 1)
+# my_cols <- viridis_pal(begin = 1,
+#                        end = 0,
+#                        option = "A")(nlev)
+
+
+# rasterVis::vectorplot(raster::stack(mean_zon2_JAS, mean_mer2_JAS),
+#                isField = 'dXY',
+#                narrows = 800,
+#                lwd.arrows = 1,
+#                aspX = 0.4,
+#                region = mean_speed2_JAS,
+#                at = my_at,
+#                col.regions = my_cols,
+#                # scales = list(cex = 1.5),
+#                colorkey = list(labels = list(cex = 2)),
+#                main = list("JAS 2018",
+#                            cex = 2.5),
+#                xlab = list("Longitude", 
+#                            cex = 2.5),
+#                ylab = list("Latitude",
+#                            cex = 2.5))     +
+#     layer(c(sp.points(ad_gls_sp[ad_gls_sp$year_period == "JAS", ],
+#                       col = rgb(0, 0, 1, alpha = 0.9),
+#                       lwd = 2),
+#             sp.points(juv_argos_sp[juv_argos_sp$year_period == "JAS" & year(juv_argos_sp$deploy) == 2018, ],
+#                       col = "white",
+#                       lwd = 2)),
+#           sp.polygons(IndOcean_sp,
+#                       col = "grey",
+#                       fill = "white"))
