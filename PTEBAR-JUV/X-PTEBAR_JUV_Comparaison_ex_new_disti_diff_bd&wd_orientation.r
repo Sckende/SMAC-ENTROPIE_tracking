4f327e4a54620251c4e234de5d050bc8574f4729
Sckende <clrccljuhasz@gmail.com>
2023-02-24 13:48:01 +0400

    # Updates

--- PTEBAR-JUV/X-PTEBAR_JUV_Comparaison_ex_new_disti_diff_bd&wd_orientation.r
+++ PTEBAR-JUV/X-PTEBAR_JUV_Comparaison_ex_new_disti_diff_bd&wd_orientation.r
@@ -1,4 +1,4 @@
-# FIRST DF TO COMPARE 
+# ---- FIRST DF TO COMPARE ---- OLD FIGURES NO SSM ---- #### 
 comp <- readRDS("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/5-PTEBAR_argos_JUV/DATA/PTEBAR_JUV_argos_with_env_DATA_wind_dirs_n_abs_speed_2.rds")
 names(comp)
 
@@ -16,7 +16,38 @@ unique(comp2$Vessel)
 
 comp2$diff_dir <- (comp2$bird_0_360_METEO_TOWARD - comp2$wind_dir_0_360) %% 360
 
-# SECOND DF TO COMPARE
+# ---- Visualisation GLOBALE
+x11()
+hist(comp2$diff_dir,
+     freq = FALSE,
+     breaks = 36,
+     main = "diff direct° bd-wd - no SSM - GLOBAL",
+     xlab = "angle (°)")
+lines(density(comp2$diff_dir,
+              na.rm = T,
+              from = 0,
+              to = 360))
+
+# ---- Visualisation GLOBALE MENSUELLE
+
+comp_month <- split(comp2,
+                    month(comp2$Date))[2:10] # retrait du mois de janvier
+x11(); par(mfrow = c(3, 3))
+lapply(comp_month,
+       function(x) {
+           hist(x$diff_dir,
+                freq = FALSE,
+                breaks = 36,
+                main = paste("diff direct° bd-wd - no SSM - GLOBAL",
+                             unique(month(x$Date, label = TRUE, abbr = FALSE))),
+                xlab = "angle (°)")
+           lines(density(x$diff_dir,
+                         na.rm = T,
+                         from = 0,
+                         to = 360))
+       }) # VALIDATED 
+
+#### ---- SECOND DF TO COMPARE ---- NEW FIGURES WITH SSM ---- ####
 loc <- readRDS("C:/Users/ccjuhasz/Desktop/SMAC/Projet_publi/5-PTEBAR_argos_JUV/DATA/PTEBAR_JUV_aniMotum_fitted_data_env_param_bird_dir_110max.rds")
 names(loc)
 head(loc$wind_meteo_dir_loc)
@@ -37,147 +68,107 @@ loc$wind_meteo_dir0_360_200km <- ifelse(loc$wind_meteo_dir_200km >= 0,
 loc$diff_wind_bird_loc <- (loc$dir_bird_deg0_360 - loc$wind_meteo_dir0_360_loc) %% 360
 loc$diff_wind_bird_200km <- (loc$dir_bird_deg0_360 - loc$wind_meteo_dir0_360_200km) %% 360
 
+# ---- Visualisation GLOBALE
+x11()
+hist(loc$diff_wind_bird_loc,
+     freq = FALSE,
+     breaks = 36,
+     main = "diff direct° bd-wd - WITH SSM - GLOBAL",
+     xlab = "angle (°)")
+lines(density(loc$diff_wind_bird_loc,
+              na.rm = T,
+              from = 0,
+              to = 360))
+
+# ---- Visualisation GLOBALE MENSUELLE
+
+loc_month <- split(loc,
+                    month(loc$date))[2:10] # retrait du mois de janvier
+x11(); par(mfrow = c(3, 3))
+lapply(loc_month,
+       function(x) {
+           hist(x$diff_wind_bird_loc,
+                freq = FALSE,
+                breaks = 36,
+                main = paste("diff direct° bd-wd - WITH SSM - GLOBAL",
+                             unique(month(x$date, label = TRUE, abbr = FALSE))),
+                xlab = "angle (°)")
+           lines(density(x$diff_wind_bird_loc,
+                         na.rm = T,
+                         from = 0,
+                         to = 360))
+       })
+
+
+#### ---- Comparaison ---- ####
 # GLOBAL
-x11(); par(mfrow = c(1, 2))
-hist(comp2$diff_dir, freq = FALSE, breaks = 36, main = "diff direct° bd-wd - no SSM - global", xlab = "angle (°)")
-lines(density(comp2$diff_dir, na.rm = T))
 
-hist(loc$diff_wind_bird_loc, freq = FALSE, breaks = 36, main = "diff direct° bd-wd - with SSM - global", xlab = "angle (°)")
-lines(density(loc$diff_wind_bird_loc, na.rm = T))
+hist_comp <- hist(comp2$diff_dir, plot = FALSE)
+hist_SSM <- hist(loc$diff_wind_bird_loc, plot = FALSE)
 
-# APRIL
-
-x11(); par(mfrow = c(2, 2))
-hist(comp2$bird_0_360_METEO_TOWARD[month(comp2$Date) == 4], freq = FALSE, breaks = 36)
-lines(density(comp2$bird_0_360_METEO_TOWARD[month(comp2$Date) == 4], na.rm = T))
-
-hist(comp2$wind_dir_0_360[month(comp2$Date) == 4], freq = FALSE, breaks = 36)
-lines(density(comp2$wind_dir_0_360[month(comp2$Date) == 4], na.rm = T))
-
-
-hist(loc$dir_bird_deg0_360[month(loc$date) == 4], freq = FALSE, breaks = 36)
-lines(density(loc$dir_bird_deg0_360[month(loc$date) == 4], na.rm = T))
-
-hist(loc$wind_meteo_dir0_360_loc[month(loc$date) == 4], freq = FALSE, breaks = 36)
-lines(density(loc$wind_meteo_dir0_360_loc[month(loc$date) == 4], na.rm = T))
-
+x11()
+hist(comp2$diff_dir,
+     freq = FALSE,
+     breaks = 36,
+     col = "grey",
+     main = "diff direct° bird-wind",
+     xlab = "angle (°)",
+     ylim = c(0, max(c(hist_comp$density, hist_SSM$density)) + 0.001))
+lines(density(comp2$diff_dir,
+              na.rm = T,
+              from = 0,
+              to = 360))
 # ---- #
-length(comp2$bird_0_360_METEO_TOWARD[month(comp2$Date) == 4])
-length(loc$dir_bird_deg0_360[month(loc$date) == 4])
+hist(loc$diff_wind_bird_loc,
+     freq = FALSE,
+     breaks = 36,
+     add = TRUE,
+     col = "#0baea0aa")
+lines(density(loc$diff_wind_bird_loc,
+              na.rm = T,
+              from = 0,
+              to = 360),
+      col = "#034d3c")
+legend("topright",
+       legend = c("NO SSM", "WITH SSM"),
+       fill = c("grey", "#0baea0aa"),
+       border = c("grey", "#0baea0aa"),
+       bty = "n")
 
+# GLOBAL MONTHLY
+
+x11(); par(mfrow = c(3, 3))
+
+for(i in 1:length(comp_month)) {
+    
+    hist_comp <- hist(comp_month[[i]]$diff_dir, plot = FALSE)
+    hist_SSM <- hist(loc_month[[i]]$diff_wind_bird_loc, plot = FALSE)
+    
+    hist(comp_month[[i]]$diff_dir,
+     freq = FALSE,
+     breaks = 36,
+     col = "grey",
+     main = paste("diff direct° bird-wind", unique(month(comp_month[[i]]$Date, label = TRUE, abbr = FALSE))),
+     xlab = "angle (°)",
+     ylim = c(0, 0.008))
+lines(density(comp_month[[i]]$diff_dir,
+              na.rm = T,
+              from = 0,
+              to = 360))
 # ---- #
-unique(comp2$Vessel)
-unique(loc$id) %in% unique(comp2$Vessel)
-
-# MAY
-
-x11(); par(mfrow = c(2, 2))
-hist(comp2$bird_0_360_METEO_TOWARD[month(comp2$Date) == 5], freq = FALSE, breaks = 36)
-lines(density(comp2$bird_0_360_METEO_TOWARD[month(comp2$Date) == 5], na.rm = T))
-
-hist(comp2$wind_dir_0_360[month(comp2$Date) == 5], freq = FALSE, breaks = 36)
-lines(density(comp2$wind_dir_0_360[month(comp2$Date) == 5], na.rm = T))
-
-
-hist(loc$dir_bird_deg0_360[month(loc$date) == 5], freq = FALSE, breaks = 36)
-lines(density(loc$dir_bird_deg0_360[month(loc$date) == 5], na.rm = T))
-
-hist(loc$wind_meteo_dir0_360_loc[month(loc$date) == 5], freq = FALSE, breaks = 36)
-lines(density(loc$wind_meteo_dir0_360_loc[month(loc$date) == 5], na.rm = T))
-
-
-# JUNE
-
-x11(); par(mfrow = c(2, 2))
-hist(comp2$bird_0_360_METEO_TOWARD[month(comp2$Date) == 6], freq = FALSE, breaks = 36)
-lines(density(comp2$bird_0_360_METEO_TOWARD[month(comp2$Date) == 6], na.rm = T))
-
-hist(comp2$wind_dir_0_360[month(comp2$Date) == 6], freq = FALSE, breaks = 36)
-lines(density(comp2$wind_dir_0_360[month(comp2$Date) == 6], na.rm = T))
-
-
-hist(loc$dir_bird_deg0_360[month(loc$date) == 6], freq = FALSE, breaks = 36)
-lines(density(loc$dir_bird_deg0_360[month(loc$date) == 6], na.rm = T))
-
-hist(loc$wind_meteo_dir0_360_loc[month(loc$date) == 6], freq = FALSE, breaks = 36)
-lines(density(loc$wind_meteo_dir0_360_loc[month(loc$date) == 6], na.rm = T))
-
-# JULY
-
-x11(); par(mfrow = c(2, 2))
-hist(comp2$bird_0_360_METEO_TOWARD[month(comp2$Date) == 7], freq = FALSE, breaks = 36)
-lines(density(comp2$bird_0_360_METEO_TOWARD[month(comp2$Date) == 7], na.rm = T))
-
-hist(comp2$wind_dir_0_360[month(comp2$Date) == 7], freq = FALSE, breaks = 36)
-lines(density(comp2$wind_dir_0_360[month(comp2$Date) == 7], na.rm = T))
-
-
-hist(loc$dir_bird_deg0_360[month(loc$date) == 7], freq = FALSE, breaks = 36)
-lines(density(loc$dir_bird_deg0_360[month(loc$date) == 7], na.rm = T))
-
-hist(loc$wind_meteo_dir0_360_loc[month(loc$date) == 7], freq = FALSE, breaks = 36)
-lines(density(loc$wind_meteo_dir0_360_loc[month(loc$date) == 7], na.rm = T))
-
-# AUGUST
-
-x11(); par(mfrow = c(2, 2))
-hist(comp2$bird_0_360_METEO_TOWARD[month(comp2$Date) == 8], freq = FALSE, breaks = 36)
-lines(density(comp2$bird_0_360_METEO_TOWARD[month(comp2$Date) == 8], na.rm = T))
-
-hist(comp2$wind_dir_0_360[month(comp2$Date) == 8], freq = FALSE, breaks = 36)
-lines(density(comp2$wind_dir_0_360[month(comp2$Date) == 8], na.rm = T))
-
-
-hist(loc$dir_bird_deg0_360[month(loc$date) == 8], freq = FALSE, breaks = 36)
-lines(density(loc$dir_bird_deg0_360[month(loc$date) == 8], na.rm = T))
-
-hist(loc$wind_meteo_dir0_360_loc[month(loc$date) == 8], freq = FALSE, breaks = 36)
-lines(density(loc$wind_meteo_dir0_360_loc[month(loc$date) == 8], na.rm = T))
-
-
-# utilisation de adehabitat pour calculer abs.angle par burst comme pour la premiere methode
-
-ar <- loc
-# hist(table(ar$point.group))
-
-#### Retrieve the UTM coordinates from sp object
-ar_sp <- SpatialPoints(ar[, c("lon", "lat")],
-                          proj4string = CRS("+proj=longlat"))
-# obtention UTM coord
-ar_UTM <- spTransform(ar_sp,
-                         CRS("+init=epsg:32743"))
-
-ar_UTM_sf <- st_as_sf(ar_UTM)
-
-coords <- st_coordinates(ar_UTM_sf)
-coords.DF <- as.data.frame(coords)
-names(coords.DF) <- c('X', 'Y')
-
-#### Complete coordinates in dataframe
-ar <- cbind(as.data.frame(ar)[, - length(names(ar))], coords.DF)
-
-#### Specific to the work on a single device ####
-# Deletion of unused factor level
-ar <- droplevels(ar)
-
-
-#### Conversion to ltraj object ####
-ar.traj <- as.ltraj(xy = ar[, c('X', 'Y')], # UTM coordinates for meter unitin distance computation
-               date = ar$date,
-               id = ar$id) # Trajectory split based on the burst of points
-
-ar.traj
-names(ar.traj[[1]])
-loc_ade <- do.call("rbind", ar.traj)
-
-loc_ade$dir_bird_deg <- loc_ade$abs.angle*180/pi
-summary(loc_ade$dir_bird_deg)
-
-loc_ade$dir_bird_deg0_360 <- ifelse(loc_ade$dir_bird_deg >= 0,
-                                loc_ade$dir_bird_deg,
-                                360 + loc_ade$dir_bird_deg)
-summary(loc_ade$dir_bird_deg0_360)
-
-x11(); par(mfrow = c(1, 2))
-hist(loc_ade$dir_bird_deg0_360)
-hist(loc$dir_bird_deg0_360)
+hist(loc_month[[i]]$diff_wind_bird_loc,
+     freq = FALSE,
+     breaks = 36,
+     add = TRUE,
+     col = "#0baea0aa")
+lines(density(loc_month[[i]]$diff_wind_bird_loc,
+              na.rm = T,
+              from = 0,
+              to = 360),
+      col = "#034d3c")
+legend("topleft",
+       legend = c("NO SSM", "WITH SSM"),
+       fill = c("grey", "#0baea0aa"),
+       border = c("grey", "#0baea0aa"),
+       bty = "n")
+}
